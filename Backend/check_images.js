dc88bb9ed8d01185b321a88f4cb0f35ea72bc6fe
DiyaJain258 <161020668+DiyaJain258@users.noreply.github.com>
2026-08-06 11:22:24 +0530

    new

--- Backend/check_images.js
+++
@@ -1,8 +0,0 @@
-const pool = require('./src/database/connection');
-
-async function check() {
-  const [cols] = await pool.query("DESCRIBE menu_items");
-  console.log('Columns:', cols.filter(c => c.Field === 'image'));
-  process.exit();
-}
-check();
--- Backend/check_users.js
+++
@@ -1,8 +0,0 @@
-const pool = require('./src/database/connection');
-
-async function check() {
-  const [cols] = await pool.query("DESCRIBE users");
-  console.log('Users columns:', cols.map(c => c.Field));
-  process.exit();
-}
-check();
